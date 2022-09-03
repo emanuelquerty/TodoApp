d8eadab7caf2f73bf0fb3777c821f1bbd05afbe4
emanuelquerty <emanuelinacio1@hotmail.com>
2022-09-03 04:41:02 -0700

    Improves filter todos feature

--- src/components/TodosInfo.svelte
+++ src/components/TodosInfo.svelte
@@ -1,12 +1,19 @@
 <script>
-  import { todoStore } from "../store";
+  import { todoStore, filterStore } from "../store";
 
   $: completedCount = $todoStore.filter((todo) => todo.completed).length;
+  $: activeCount = $todoStore.length - completedCount;
+  $: itemsCount = $todoStore.length;
 </script>
 
 <h2>
-  {completedCount} out of {$todoStore.length}
-  {$todoStore.length > 1 ? "items" : "item"} completed
+  {#if $filterStore === "all" || $filterStore === "completed"}
+    {completedCount} out of {itemsCount}
+    {itemsCount > 1 ? "items" : "item"} completed
+  {:else}
+    {activeCount} out of {itemsCount}
+    {itemsCount > 1 ? "items" : "item"} active
+  {/if}
 </h2>
 
 <style>
